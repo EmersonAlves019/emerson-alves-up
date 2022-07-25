33de8451bbc8ab95f9c5b7185feaeb918e96479c
EmersonAlves <emersondasilva.alves99@gmail.com>
2022-07-25 15:03:04 -0300

    feat: configurando variáveis de ambiente e Dockerfile

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -10,13 +11,14 @@ const app = express();
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
+
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
